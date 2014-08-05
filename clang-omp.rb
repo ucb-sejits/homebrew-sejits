4187de5415e08f30ebb8de66399452d99d42a11e
Leonard Truong <leonardtruong@gmail.com>
2014-08-04 19:36:11 -0700

    Initial commit of working clang-omp formula

---
+++ clang-omp.rb
@@ -0,0 +1,26 @@
+require "formula"
+
+class ClangOmp < Formula
+  homepage ""
+  head "https://github.com/clang-omp/llvm", :using => :git
+  sha1 ""
+
+  keg_only :provided_by_osx, "OS X provides an older sqlite3."
+
+  def install
+
+    system "git", "clone", "https://github.com/clang-omp/compiler-rt",
+           "projects/compiler-rt"
+    system "git", "clone", "-b", "clang-omp",
+           "https://github.com/clang-omp/clang", "tools/clang"
+
+    system "./configure", "--enable-optimized",
+                          "--prefix=#{prefix}"
+    ENV['REQUIRES_RTTI'] = '1'
+    system "make", "install"
+  end
+
+  test do
+    system "true"
+  end
+end
